028afcd05d8c553113ed88563cc4b801340fb8bb
Anton Kolesynskyi <antony@macbook-pro-antony.local>
2019-01-27 03:40:48 +0100

    SecondVC code updating

--- TestTaskProject/MainViewController.swift
+++ TestTaskProject/MainViewController.swift
@@ -12,34 +12,6 @@ import Alamofire
 
 typealias JSONDict = [String: Any]
 
-extension Dictionary where Key == String {
-    func string(_ key: String) -> String? {
-        if let value = self[key] as? String {
-            return value
-        } else {
-            print("Missing parameter '\(key)'")
-            return nil
-        }
-    }
-    
-    func array(_ key: String) -> [JSONDict] {
-        if let value = self[key] as? [JSONDict] {
-            return value
-        } else {
-            print("Missing parameter '\(key)'")
-            return []
-        }
-    }
-    
-    func dict(_ key: String) -> JSONDict? {
-        if let value = self[key] as? JSONDict{
-            return value
-        } else {
-            print("Missing parameter '\(key)'")
-            return nil
-        }
-    }
-}
 
 
 var selectedIndexPath = Int()
@@ -98,6 +70,35 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
 }
+
+    extension Dictionary where Key == String {
+        func string(_ key: String) -> String? {
+            if let value = self[key] as? String {
+                return value
+            } else {
+                print("Missing parameter '\(key)'")
+                return nil
+            }
+        }
+        
+        func array(_ key: String) -> [JSONDict] {
+            if let value = self[key] as? [JSONDict] {
+                return value
+            } else {
+                print("Missing parameter '\(key)'")
+                return []
+            }
+        }
+        
+        func dict(_ key: String) -> JSONDict? {
+            if let value = self[key] as? JSONDict{
+                return value
+            } else {
+                print("Missing parameter '\(key)'")
+                return nil
+            }
+        }
+    }
 
 
 
--- TestTaskProject/SecondViewController.swift
+++ TestTaskProject/SecondViewController.swift
@@ -11,8 +11,42 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var firstUserName: UILabel!
+    @IBOutlet weak var lastUserName: UILabel!
+    @IBOutlet weak var userGender: UILabel!
+    @IBOutlet weak var userDateOfBirth: UILabel!
+    @IBOutlet weak var userPhoneNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.title = "Screen title"
+        
+        let jsonDate = users[selectedIndexPath].dateOfBirth
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "YYYY-mm-dd"
+        
+        if let date = dateFormatterGet.date(from: jsonDate) {
+            userDateOfBirth.text = dateFormatterPrint.string(from: date)
+        } else {
+            print("Error")
+        }
+        
+        firstUserName.text = users[selectedIndexPath].first
+        lastUserName.text = users[selectedIndexPath].last
+        userGender.text = users[selectedIndexPath].gender
+        userPhoneNumber.text = users[selectedIndexPath].phone
+        
+        let imageURL = users[selectedIndexPath].mediumPicture
+        if let ImageData = NSData(contentsOf: imageURL) {
+            userImage.image = UIImage(data: ImageData as Data)
+        }
+        userImage.layer.cornerRadius = 75.0
+        userImage.clipsToBounds = true
+        
     }
     
 }
